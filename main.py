be1080e5c4b0f1cd21af8e64d930653ddbd36906
sashavershkova <a.m.vershkova@gmail.com>
2025-03-18 15:15:08 -0700

    Passed all the tests

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 def calculate_class_averages(classrooms):
-    pass
+    ave_dict = {}
+
+    for subject in classrooms:        
+        all_grades = []
+        if not classrooms[subject]:
+            ave_dict[subject] = 0
+        else:
+            for student_grades in classrooms[subject].values():
+                all_grades += student_grades   
+        
+            sum_subj_grades = sum(all_grades)
+            count = len(all_grades)
+
+            ave_dict[subject] = sum_subj_grades/count
+
+    return ave_dict
+
+
+
+
+
+
+    
